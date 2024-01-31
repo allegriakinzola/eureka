9bba2cc2d20b93fe4110aa1786e26f269df75976
allegria25 <allegriakinzoladev@gmail.com>
2024-01-31 08:45:19 +0100

    commit

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -1,15 +1,18 @@
-import { FaPerson } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+
 
 export function Footer (){
     return(
         <div className="footer">
            <div className="footercontent">
                 <div className="listicone">
-                    <span><FaPerson/></span>
-                    <span><FaPerson/></span>
-                    <span><FaPerson/></span>
+                    <a href="https://github.com/allegriakinzola" target="_blank"><span><FaGithub/></span></a>
+                    <a href="https://github.com/allegriakinzola" target="_blank"><span><FaWhatsapp/></span></a>
+                    <a href="https://github.com/allegriakinzola" target="_blank"><span><CiLinkedin/></span></a>
                 </div>
-                <p>&copy; 2022 Votre Allegria Kinzola. Tous droits réservés.</p>
+                <p>&copy; 2023 Allegria Kinzola. Tous droits réservés.</p>
            </div>
         </div>
     )
